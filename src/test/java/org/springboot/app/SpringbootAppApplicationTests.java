fb82818cd1da88b5466f186b79d0f0f2c0a2119a
Cristian Blandon <cristianblandon13@gmail.com>
2021-09-19 14:36:17 -0500

    Verifying method invocation

--- src/test/java/org/springboot/app/SpringbootAppApplicationTests.java
+++ src/test/java/org/springboot/app/SpringbootAppApplicationTests.java
@@ -5,6 +5,8 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springboot.app.models.Banco;
+import org.springboot.app.models.Cuenta;
 import org.springboot.app.repositories.BancoRepositorio;
 import org.springboot.app.repositories.CuentaRepositorio;
 import org.springboot.app.services.CuentaService;
@@ -16,35 +18,46 @@ import java.math.BigDecimal;
 @SpringBootTest
 class SpringbootAppApplicationTests {
 
-	CuentaRepositorio cuentaRepositorio;
-	BancoRepositorio bancoRepositorio;
+    CuentaRepositorio cuentaRepositorio;
+    BancoRepositorio bancoRepositorio;
 
-	CuentaService service;
+    CuentaService service;
 
-	@BeforeEach
-	void setUp() {
-		cuentaRepositorio = mock(CuentaRepositorio.class);
-		bancoRepositorio = mock(BancoRepositorio.class);
-		service = new CuentaServiceImpl(cuentaRepositorio, bancoRepositorio);
-	}
+    @BeforeEach
+    void setUp() {
+        cuentaRepositorio = mock(CuentaRepositorio.class);
+        bancoRepositorio = mock(BancoRepositorio.class);
+        service = new CuentaServiceImpl(cuentaRepositorio, bancoRepositorio);
+    }
 
-	@Test
-	void contextLoads() {
-		when(cuentaRepositorio.findById(1L)).thenReturn(Datos.CUENTA_001);
-		when(cuentaRepositorio.findById(2L)).thenReturn(Datos.CUENTA_002);
-		when(bancoRepositorio.findById(1L)).thenReturn(Datos.BANCO);
+    @Test
+    void contextLoads() {
+        when(cuentaRepositorio.findById(1L)).thenReturn(Datos.CUENTA_001);
+        when(cuentaRepositorio.findById(2L)).thenReturn(Datos.CUENTA_002);
+        when(bancoRepositorio.findById(1L)).thenReturn(Datos.BANCO);
 
-		BigDecimal saldoOrigen = service.revisarSaldo(1L);
-		BigDecimal saldoDestino = service.revisarSaldo(2L);
-		assertEquals("1000", saldoOrigen.toPlainString());
-		assertEquals("2000", saldoDestino.toPlainString());
+        BigDecimal saldoOrigen = service.revisarSaldo(1L);
+        BigDecimal saldoDestino = service.revisarSaldo(2L);
+        assertEquals("1000", saldoOrigen.toPlainString());
+        assertEquals("2000", saldoDestino.toPlainString());
 
-		service.transferir(1L, 2L, new BigDecimal("100"), 1L);
-		saldoOrigen = service.revisarSaldo(1L);
-		saldoDestino = service.revisarSaldo(2L);
+        service.transferir(1L, 2L, new BigDecimal("100"), 1L);
+        saldoOrigen = service.revisarSaldo(1L);
+        saldoDestino = service.revisarSaldo(2L);
 
-		assertEquals("900", saldoOrigen.toPlainString());
-		assertEquals("2100", saldoDestino.toPlainString());
-	}
+        assertEquals("900", saldoOrigen.toPlainString());
+        assertEquals("2100", saldoDestino.toPlainString());
+
+		int total = service.revisarTotalTranferencias(1L);
+		assertEquals(1, total);
+
+        verify(cuentaRepositorio, times(3)).findById(1L);
+        verify(cuentaRepositorio, times(3)).findById(2L);
+        verify(cuentaRepositorio, times(2)).update(any(Cuenta.class));
+
+		verify(bancoRepositorio, times(2)).findById(1L);
+        verify(bancoRepositorio).update(any(Banco.class));
+
+    }
 
 }
